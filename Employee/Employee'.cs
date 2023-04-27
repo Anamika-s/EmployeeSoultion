6fb90f27f9f204e3f7551f637f05ffcf7247e691
Anamika-s <anamika.sawhney22@gmail.com>
2023-04-27 12:48:55 +0530

    Add project files.

---
+++ Employee/Employee'.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ 
+    internal class Employee
+    {
+          int id;
+        string name;
+    static string dept;
+        decimal salary;
+          string manager;
+    string company;
+    public Employee()
+    {
+
+    }
+       public Employee(string name)
+    {
+        this.name = name;
+        Console.WriteLine("ENter Id");
+        id = Byte.Parse(Console.ReadLine());
+        
+        //Console.WriteLine("ENter Dept");
+        //dept = Console.ReadLine();
+        Console.WriteLine("ENter Manager");
+        manager = Console.ReadLine();
+        Console.WriteLine("ENter Company");
+        company = Console.ReadLine();
+        Console.WriteLine("ENter Salary");
+        salary = Convert.ToDecimal(Console.ReadLine());
+
+
+    }
+    public Employee(int id, string name)
+    {
+        this.name = name;
+        this.id = id;
+       
+
+        //Console.WriteLine("ENter Dept");
+        //dept = Console.ReadLine();
+        Console.WriteLine("ENter Manager");
+        manager = Console.ReadLine();
+        Console.WriteLine("ENter Company");
+        company = Console.ReadLine();
+        Console.WriteLine("ENter Salary");
+        salary = Convert.ToDecimal(Console.ReadLine());
+
+
+    }
+
+    public Employee(Employee emp)
+    {
+        Console.WriteLine("ENter Id");   
+        id = Byte.Parse(Console.ReadLine());
+        this.name = emp.name;
+        this.manager = emp.manager;
+       
+        this.salary = emp.salary;
+
+
+    }
+
+    static Employee()
+    {
+        dept = "HR";
+    }
+    public void GetDetails()
+        {
+            `Console.WriteLine("ENter Id");
+            id = Byte.Parse(Console.ReadLine());
+            Console.WriteLine("ENter NAme");
+            name = Console.ReadLine();
+            //Console.WriteLine("ENter Dept");
+            //dept = Console.ReadLine();
+            //Console.WriteLine("ENter Manager");
+            //manager = Console.ReadLine();
+            Console.WriteLine("ENter Company");
+            company = Console.ReadLine();
+            Console.WriteLine("ENter Salary");
+            salary = Convert.ToDecimal(Console.ReadLine());
+        }
+         public void DisplayDetails()
+        {
+
+            Console.WriteLine("ID is {0}", id);
+            Console.WriteLine("NAme is {0}", name);
+            Console.WriteLine("Dept is {0}", dept);
+            Console.WriteLine("Manager is {0}", manager);
+            Console.WriteLine("COmpmay is {0}", company);
+            Console.WriteLine("Salary is {0}", salary);
+        }
+
+        }
+
+    
+
---
+++ Employee/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EmployeeProject
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Employee.dept = "HR";
+            //Employee.Department();
+            //Employee employee = new Employee();
+            //employee.GetDetails();
+            //employee.DisplayDetails();
+            
+            Employee employee1 = new Employee();
+            employee1.GetDetails();
+            employee1.DisplayDetails();
+
+            Employee employee2 = new Employee(name:"Ajay");
+            employee2.DisplayDetails();
+
+            Employee employee3 = new Employee(id:100, name:"Lalit");
+
+            employee3.DisplayDetails();
+            Employee employee4 = new Employee(employee1);
+        }
+    }
+}
